b03f1f08ac427b6eff4bc923651fe4242fdb83d9
Michael Kaiser <michael.kaiser@conplement.de>
2021-11-12 15:33:17 +0100

    adds vue project with windi css

---
+++ vue/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </div>
+</template>
+
+<style>
+</style>
