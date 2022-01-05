fa9d252da24940adaaad6507257b3fe8472a82fe
Michael Kaiser <michael.kaiser@conplement.de>
2022-01-05 14:23:06 +0100

    adds template ref example

--- react/src/components/Movies.tsx
+++ react/src/components/Movies.tsx
@@ -1,21 +1,29 @@
+import { useRef } from 'react'
 import { useMovies } from '../hooks/useMovies'
 import { TMDB } from '../models/movie'
 import { MovieCard } from './MovieCard'
 
 export const Movies = () => {
+  const searchBox = useRef<HTMLInputElement | null>(null)
   const { movies, searchText, setSearchText, resultCount } = useMovies()
+
+  const focusInput = () => searchBox.current!.select()
 
   return (
     <div className={`flex flex-col items-center mt-5`}>
-      <div className="py-4 flex flex-col">
+      <div className="py-4 flex flex-col gap-3">
         <input
+          ref={searchBox}
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
           type="text"
           className="p-3 w-80 ring-2 ring-gray-600 bg-transparent focus:outline-none focus:ring-3 rounded-lg"
           placeholder="search movies and tv shows..."
         />
-        <span className="mt-1">{resultCount}</span>
+        <button onClick={focusInput} className="bg-gray-600 border-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 border rounded-lg">
+          Focus the search
+        </button>
+        <span>Results: {resultCount}</span>
       </div>
       <div className="flex gap-6 mt-10 flex-wrap px-20">
         {movies.map((m: TMDB.Movie) => (
--- vue/src/components/Movies.vue
+++ vue/src/components/Movies.vue
@@ -1,21 +1,30 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import { useMovies } from '../composables/useMovies';
 import MovieCard from './MovieCard.vue';
 
+const searchBox = ref<HTMLInputElement | null>(null)
 const { movies, searchText, resultCount } = useMovies()
+
+const focusInput = () => searchBox.value!.select()
 
 </script>
 
 <template>
     <div class="flex flex-col items-center">
-        <div class="py-4 flex flex-col">
+        <div class="py-4 flex flex-col gap-3">
             <input
+                ref="searchBox"
                 v-model="searchText"
                 type="text"
                 class="p-3 w-80 ring-2 ring-gray-600 bg-transparent focus:outline-none focus:ring-3 rounded-lg"
                 placeholder="search movies and tv shows..."
             />
-            <span class="mt-1">Results: {{ resultCount }}</span>
+            <button
+                @click="focusInput"
+                class="bg-gray-600 border-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 border rounded-lg"
+            >Focus the search</button>
+            <span>Results: {{ resultCount }}</span>
         </div>
         <div class="flex gap-6 mt-5 flex-wrap mx-20">
             <MovieCard v-for="m in movies" :key="m.id" :movie="m" />
